1035a525be5a6892d5123aa80cdf7539de3af45a
Mohamed Abdullahi <112027511+Mohamed-code-ship@users.noreply.github.com>
2023-04-05 09:29:04 +0300

    Update settings.py

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-todo-app-production-e5de.up.railway.app'
+]
